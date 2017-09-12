8c0b84626e8d170235ece096f06edf5afdc66dfa
DanielSrb <srb.dan@gmail.com>
2017-09-12 20:09:18 +0200

    REFACTOR: whitespaces + comments

--- gulp-templates/02-scss/gulpfile.js
+++ gulp-templates/02-scss/gulpfile.js
@@ -8,29 +8,29 @@ var sourcemaps = require('gulp-sourcemaps');
 
 // a task to delete all css files in main folder
 gulp.task('css:clean', function(){
-	return del(['*.css'], { force: true });
+    return del(['*.css'], { force: true });
 });
 
 // CSS compilation (also deletes css files first using previously defined task)
 gulp.task('css:compile', ['css:clean'], function(){
-	return gulp
-		.src('index.scss') // this is the source of for compilation from scss to css
-		.pipe(sass().on('error', sass.logError)) // compile sass to css and also tell us about a problem if happens
-		.pipe(sourcemaps.init()) // creates a sourcemap
-		.pipe(sourcemaps.write('.')) // writes the sourcemap
-		.pipe(gulp.dest('./')) // destination of the resulting css
-		.pipe(browserSync.stream()); // tell browsersync to reload CSS (injects compiled CSS)
+    return gulp
+        .src('index.scss') // this is the source of for compilation from scss to css
+        .pipe(sass().on('error', sass.logError)) // compile sass to css and also tell us about a problem if happens
+        .pipe(sourcemaps.init()) // initalizes a sourcemap
+        .pipe(sourcemaps.write('.')) // writes the sourcemap
+        .pipe(gulp.dest('./')) // destination of the resulting css
+        .pipe(browserSync.stream()); // tell browsersync to reload CSS (injects compiled CSS)
 });
 
 gulp.task('develop', ['css:compile'], function(){
-	browserSync.init({ // initalize Browsersync
-		// set what files be served
-		server: {
-			baseDir: './' // serve from folder that this file is located
-		},
-		files: ['*.html'] // watch for changes all files named anything.html in the same folder gulpfile.js is located
-	});
-	gulp.watch('*.scss', ['css:compile']); // watch for changes in scss files
+    browserSync.init({ // initalize Browsersync
+        // set what files be served
+        server: {
+            baseDir: './' // serve from folder that this file is located
+        },
+        files: ['*.html'] // watch for changes all files named anything.html in the same folder gulpfile.js is located
+    });
+    gulp.watch('*.scss', ['css:compile']); // watch for changes in scss files
 });
 
 gulp.task('default', ['develop']); // set develop as a default task (gulp runs this when you don't specify a task)
